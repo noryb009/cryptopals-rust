5327cbe40d581b9074e96c4338bb21650b7d46b9
Luke Lorimer <noryb009@gmail.com>
2025-12-03 22:07:22 -0800

    Commit current state

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod base64;
+mod hex;
+
 fn main() {
     println!("Hello, world!");
+    println!("{:?}", hex::hex_to_bin("0123456789abcdef").unwrap());
+    println!("{}", hex::bin_to_hex(&hex::hex_to_bin("0123456789abcdef").unwrap()));
 }
